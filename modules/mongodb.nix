c54eceaa7d72fb96dc0b99d99c64a13e9b96c100
Skyler Grey <skyler3665@gmail.com>
2023-03-03 23:19:46 +0000

    Install mongosh, move services to modules

--- modules/mongodb.nix
+++ modules/mongodb.nix
@@ -1,4 +1,5 @@
 { config, pkgs, ... }: {
+  environment.systemPackages = [ pkgs.mongosh ];
   services.mongodb.enable = true;
   services.mongodb.enableAuth = true;
   services.mongodb.initialRootPassword = "fYhw&%6frpcL9zcJ5p^b^tquP0kyVE9hehoLY4lY2zUUzbIjEyDPhAIMe2M";
