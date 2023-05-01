8b0e52f566d01a4cd6a9e7ba1a3e07d9cbbe4001
Skyler Grey <skyler3665@gmail.com>
2023-05-01 21:31:07 +0000

    Make caddy use caddy.json

--- modules/caddy.nix
+++ modules/caddy.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, lib, ... }: {
   services.caddy.enable = true;
-  services.caddy.extraConfig = builtins.readFile ./caddy/Caddyfile;
+  services.caddy.configFile = ./caddy/caddy.json;
+  services.caddy.package = pkgs.callPackage ../packages/caddy.nix {
+    vendorSha256 = "sha256-3KcoOAB+YkOU8qKM75uQo58/dljRBmP25dionr9r2dc=";
+  };
+  services.caddy.user = "root";
+  systemd.services.caddy.serviceConfig.ProtectHome = lib.mkForce false;
 }
