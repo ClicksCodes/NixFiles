629d2bb811ea2854cf863fab6f180f3dca24da17
Skyler Grey <skyler3665@gmail.com>
2023-05-12 00:28:27 +0000

    Allow hopescaramels to access mongo through the firewall

--- modules/mongodb.nix
+++ modules/mongodb.nix
@@ -5,4 +5,6 @@
   services.mongodb.bind_ip = "0.0.0.0";
   services.mongodb.initialRootPassword = "changeme";
   services.mongodb.package = pkgs.mongodb-6_0;
+
+  networking.firewall.extraCommands = "iptables -A INPUT -s 192.168.0.4 -p tcp --dport 27017 -j ACCEPT";
 }
